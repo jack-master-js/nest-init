e9073f9e372c11bf5ba9643fdf6a0278c5a96c16
paddy <patricknieh@hotmail.com>
2021-02-13 12:58:24 +0800

    process

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//Client --->Middleware--->Guard--->Interceptor--->Pipe--->Controller--->Interceptor--->Filter
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
