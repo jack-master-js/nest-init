81867bf7b1ddfe23bcc772ee296d5eefc847e626
paddy <patricknieh@hotmail.com>
2021-02-13 13:02:47 +0800

    interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//Client --->Middleware--->Guard--->Interceptor--->Pipe--->Controller--->Interceptor--->Filter
+//Client --->Middleware--->Guard--->Interceptor Before--->Pipe--->Controller--->Interceptor After--->Filter
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
