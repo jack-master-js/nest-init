c87af072e9f484b33bf4e06e043c6e36e7b1cb09
paddy <patricknieh@hotmail.com>
2021-02-25 11:05:52 +0800

    count

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,37 @@
-import { Controller, Get, Query, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UseInterceptors,
+  Inject,
+  Post,
+  Body,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { ValidationPipe } from './validation.pipe';
 import { Roles } from './roles.decorator';
 import { LoggingInterceptor } from './logging.interceptor';
+import { Observable } from 'rxjs';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Controller()
 @UseInterceptors(LoggingInterceptor)
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @Inject('MATH_SERVICE') private client: ClientProxy,
+    private readonly appService: AppService,
+  ) {}
 
   @Get()
   @Roles('admin')
   getHello(@Query(new ValidationPipe()) id): string {
     return this.appService.getHello();
+  }
+
+  @Post('/math/wordcount')
+  wordCount(
+    @Body() { text }: { text: string },
+  ): Observable<{ [key: string]: number }> {
+    return this.client.send('math:wordcount', text);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { APP_GUARD } from '@nestjs/core';
 import { RolesGuard } from './roles.guard';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      { name: 'MATH_SERVICE', transport: Transport.TCP },
+      //   {
+      //     name: 'MATH_SERVICE',
+      //     transport: Transport.REDIS,
+      //     options: {
+      //       url: 'redis://:root@localhost:16379',
+      //     },
+      //   },
+    ]),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
